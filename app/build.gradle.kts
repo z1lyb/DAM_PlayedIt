c236152bb3c2c9faf9ab62776e4fa21b35bcf0ad
z1lyb <zisajoia@gmail.com>
2026-05-28 01:09:36 +0100

    jetpack compose and firebase necessary imports

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,11 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" // JPC
+    // Firebase
+    //id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -20,6 +26,12 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    // Import API key to be used in project
+    val localProperties = rootProject.file("local.properties")
+    if(localProperties.exists()) {
+        localProperties.inputStream().use { Properties().load(it) }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -33,6 +45,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    // Jetpack Compose
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
@@ -44,4 +61,28 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+
+    // JSON
+    implementation("com.google.code.gson:gson:2.8.9")
+
+    // Jetpack Compose
+    val composeBom = platform("androidx.compose:compose-bom:2025.01.00")
+    implementation(composeBom)
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("androidx.activity:activity-compose:1.10.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+    implementation("androidx.compose.material:material-icons-extended:1.7.0")
+
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:34.13.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
 }
---
+++ app/src/main/java/dam_51606/playedit/ui/theme/Themes.kt
@@ -0,0 +1,4 @@
+package dam_51606.playedit.ui.theme
+
+class Themes {
+}
